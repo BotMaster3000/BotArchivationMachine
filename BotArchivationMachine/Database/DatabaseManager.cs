26c21adb751c6503e4d6b9b99abcda59f75f78e5
BotMaster3000 <jdgebauer@gmx.de>
2019-03-07 23:46:57 +0100

    Added DatabaseManager-Test for GetAllEntries-Method and implemented it. Also Added SQLiteCore-Package to the UnitTest-Project

--- BotArchivationMachine/Database/DatabaseManager.cs
+++ BotArchivationMachine/Database/DatabaseManager.cs
@@ -4,11 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BotArchivationMachine.Interfaces;
+using System.Data.SQLite;
+using System.IO;
+using BotArchivationMachine.Models;
 
 namespace BotArchivationMachine.Database
 {
     public class DatabaseManager : IDatabaseManager
     {
+        private readonly SQLiteConnection connection;
+        public const string DATABASE_NAME = "ArchivationDataStorage.sqlite";
+        private const string TABLE_NAME = "ArchivationInformation";
+
+        public DatabaseManager()
+        {
+            if (!DatabaseExists())
+            {
+                CreateDataBase();
+                connection = new SQLiteConnection(GetConnectionString());
+                GenerateTables();
+            }
+            else
+            {
+                connection = new SQLiteConnection(GetConnectionString());
+            }
+        }
+
+        private bool DatabaseExists()
+        {
+            return File.Exists(DATABASE_NAME);
+        }
+
+        private void CreateDataBase()
+        {
+            SQLiteConnection.CreateFile(DATABASE_NAME);
+        }
+
+        private string GetConnectionString()
+        {
+            return $"Data Source = {DATABASE_NAME};Version = 3;";
+        }
+
+        private void GenerateTables()
+        {
+            connection.Open();
+            string tableSQL = $"CREATE TABLE {TABLE_NAME} (ID INT, Path VARCHAR, LastArchivingDate DATETIME, ArchivingIntervalInHours INT)";
+            using (SQLiteCommand command = new SQLiteCommand(tableSQL, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+            connection.Close();
+        }
+
         public void AddEnty(IArchiveModel archive)
         {
             throw new NotImplementedException();
@@ -16,7 +63,27 @@ namespace BotArchivationMachine.Database
 
         public List<IArchiveModel> GetAllEntries()
         {
-            throw new NotImplementedException();
+            List<IArchiveModel> returnList = new List<IArchiveModel>();
+
+            connection.Open();
+            string sql = $"SELECT * FROM {TABLE_NAME}";
+            using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+            using (SQLiteDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    returnList.Add(new ArchiveModel()
+                    {
+                        ID = (int)reader["ID"],
+                        Path = (string)reader["Path"],
+                        LastArchivingDate = (DateTime)reader["LastArchivingDate"],
+                        ArchivingIntervalInHours = (int)reader["ArchivingIntervalInHours"],
+                    });
+                }
+            }
+            connection.Close();
+
+            return returnList;
         }
 
         public void RemoveEntry(IArchiveModel archive)
--- BotArchivationMachineTests/Database/DatabaseManagerTests.cs
+++ BotArchivationMachineTests/Database/DatabaseManagerTests.cs
@@ -5,16 +5,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BotArchivationMachine.Interfaces;
+using System.IO;
 
 namespace BotArchivationMachine.Database.Tests
 {
     [TestClass]
     public class DatabaseManagerTests
     {
+        private void DisposeDataBase()
+        {
+            if (File.Exists(DatabaseManager.DATABASE_NAME))
+            {
+                File.Delete(DatabaseManager.DATABASE_NAME);
+            }
+        }
+
         [TestMethod]
         public void DatabaseManager_Constructor_Test()
         {
-            DatabaseManager databaseManager = new DatabaseManager();
+            try
+            {
+                DatabaseManager manager = new DatabaseManager();
+            }
+            finally
+            {
+                DisposeDataBase();
+            }
+        }
+
+        [TestMethod]
+        public void DatabaseManager_GetAllEntries_ShouldReturnNoEntries()
+        {
+            try
+            {
+                DatabaseManager manager = new DatabaseManager();
+                List<IArchiveModel> modelList = manager.GetAllEntries();
+
+                Assert.AreEqual(0, modelList.Count);
+            }
+            finally
+            {
+                DisposeDataBase();
+            }
         }
     }
 }
